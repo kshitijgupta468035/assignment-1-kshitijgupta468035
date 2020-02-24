026bae18d38d4b4fc833e034b80b3e8e60e02f5c
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-02-24 20:52:44 +0530

    using do while loop and write all printed statements

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -13,6 +13,14 @@ public class Main {
         ArrayList<String> list2 = new ArrayList<>();
 
         SingleLinkedList<Person> list = new SingleLinkedList<Person>();
-
+        boolean b = true;
+        do {
+            System.out.println("Welcome to Kshitij Gupta's Contact List Application\n" +
+                    "Press 1 to add a new contact in list\n" +
+                    "Press 2 to view all contacts of the list\n" +
+                    "Press 3 to search for a contact from the list\n" +
+                    "Press 4 to delete a contact of the list\n" +
+                    "Press 5 to exit program completely");
+        } while (b);
     }
 }
