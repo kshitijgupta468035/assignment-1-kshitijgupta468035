f076e92549c7e8ba35824d7338fde4b331f6791d
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-02-24 20:28:01 +0530

    add list to store numbers

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,11 +1,16 @@
 package person;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person implements Comparable<Person> {
 
     private String firstName;
     private String lastName;
     private String email;
     private long phoneNumber;
+
+    List<Long> list = new ArrayList<>();
 
     @Override
     public int compareTo(Person o) {
