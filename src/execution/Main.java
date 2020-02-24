0d5c7c01b4e9236037182e48d707eb7b09c6309f
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-02-24 20:44:02 +0530

    add two ArrayList first list1 secon list2

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -1,10 +1,13 @@
 package execution;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> list1 = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
 
     }
 }
