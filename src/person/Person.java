238f700795bb126f99523646b991b28f1d667286
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-02-24 20:32:54 +0530

    add getter and setter method for all but not the setter method for list

--- src/person/Person.java
+++ src/person/Person.java
@@ -23,6 +23,44 @@ public class Person implements Comparable<Person> {
         this.list = list;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<Long> getList() {
+        return list;
+    }
+
+
+
     @Override
     public int compareTo(Person o) {
         return 0;
