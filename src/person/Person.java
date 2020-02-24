479e89455fb5a7a51cb504571ee6d7377c57d1fb
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-02-24 20:27:05 +0530

    add four private variables

--- src/person/Person.java
+++ src/person/Person.java
@@ -2,6 +2,11 @@ package person;
 
 public class Person implements Comparable<Person> {
 
+    private String firstName;
+    private String lastName;
+    private String email;
+    private long phoneNumber;
+
     @Override
     public int compareTo(Person o) {
         return 0;
