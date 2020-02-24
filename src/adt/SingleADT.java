6786dde92adf14d0f23e0fb73ddd8e7cc547eaa3
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-02-24 19:16:25 +0530

    add all the methods of the interface with some description of its usage

--- src/adt/SingleADT.java
+++ src/adt/SingleADT.java
@@ -1,4 +1,24 @@
 package adt;
 
 public interface SingleADT<E> {
+    /* This method will add the item in the list. */
+
+    void add(E item);
+
+    /* This method will remove an item from the linked list. */
+
+    void remove();
+
+    // This method will search the item in the list
+
+    int search(E item);
+
+    // This method will sort the list based on there node acc. to ascending
+    //order
+
+    void sort();
+
+    // This method will print all the item in the list.
+
+    void print();
 }
