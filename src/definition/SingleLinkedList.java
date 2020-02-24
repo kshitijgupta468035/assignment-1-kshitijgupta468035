d39d6571d412ce057344f7b8522bc4f2e7b30d68
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-02-24 19:20:21 +0530

    implements SingleADT interface and Override all its methods

--- src/definition/SingleLinkedList.java
+++ src/definition/SingleLinkedList.java
@@ -1,4 +1,31 @@
 package definition;
 
-public class SingleLinkedList {
+import adt.SingleADT;
+
+public class SingleLinkedList<E> implements SingleADT<E> {
+
+    @Override
+    public void add(E item) {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public int search(E item) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
